ce7245ee83dd19becbec560eec45ff46449b467d
Alin Bodea <cinewva@gmail.com>
2024-05-09 08:02:13 +0300

    adding 1 to i and print in console every itteration

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//just printing something in console using for
+public class Main {
+    public static void main(String[] args) {
+        // greeting
+        System.out.println("Hello and welcome!");
+
+        // using for 5 times
+        for (int i = 1; i <= 5; i++) {
+
+            // adding 1 to i and print in console every itteration
+            System.out.println("i = " + (i + 1) );
+        }
+    }
+}
